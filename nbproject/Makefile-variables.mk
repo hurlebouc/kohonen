820d66b2e6a639b738e320533d93ce3d41b8cf62
hurlebouc <hurlebouc@518b536e-6de5-474b-912a-2c47fa7b0e45>
2012-04-11 18:37:47 +0000

    ajout de la version NetBeans du projet
    git-svn-id: svn+ssh://svn.code.sf.net/p/kohonen/code/trunk@7 518b536e-6de5-474b-912a-2c47fa7b0e45

---
+++ nbproject/Makefile-variables.mk
@@ -0,0 +1,35 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+CND_BUILDDIR=build
+CND_DISTDIR=dist
+# Debug configuration
+CND_PLATFORM_Debug=GNU-MacOSX
+CND_ARTIFACT_DIR_Debug=dist/Debug/GNU-MacOSX
+CND_ARTIFACT_NAME_Debug=recforme
+CND_ARTIFACT_PATH_Debug=dist/Debug/GNU-MacOSX/recforme
+CND_PACKAGE_DIR_Debug=dist/Debug/GNU-MacOSX/package
+CND_PACKAGE_NAME_Debug=recforme.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/GNU-MacOSX/package/recforme.tar
+# Release configuration
+CND_PLATFORM_Release=GNU-MacOSX
+CND_ARTIFACT_DIR_Release=dist/Release/GNU-MacOSX
+CND_ARTIFACT_NAME_Release=recforme
+CND_ARTIFACT_PATH_Release=dist/Release/GNU-MacOSX/recforme
+CND_PACKAGE_DIR_Release=dist/Release/GNU-MacOSX/package
+CND_PACKAGE_NAME_Release=recforme.tar
+CND_PACKAGE_PATH_Release=dist/Release/GNU-MacOSX/package/recforme.tar
+#
+# include compiler specific variables
+#
+# dmake command
+ROOT:sh = test -f nbproject/private/Makefile-variables.mk || \
+	(mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk)
+#
+# gmake command
+.PHONY: $(shell test -f nbproject/private/Makefile-variables.mk || (mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk))
+#
+include nbproject/private/Makefile-variables.mk

5ec74af69c4d9cac720039f0e695b84b2722cbf0
hurlebouc <mathsmac@yahoo.fr>
2012-06-12 00:10:07 +0200

    Ajout d'une classe de tests

---
+++ RecForme/Tests.cpp
@@ -0,0 +1,14 @@
+//
+//  Tests.cpp
+//  RecForme
+//
+//  Created by Hubert Godfroy on 12/06/12.
+//  Copyright (c) 2012 Mines de Nancy. All rights reserved.
+//
+
+#include <iostream>
+#include "Tests.h"
+
+using namespace std;
+
+string chemin = "/Users/hubert/Dropbox/personnel/developpeur/projet/kohonen/tests";
---
+++ RecForme/Tests.h
@@ -0,0 +1,14 @@
+//
+//  Tests.h
+//  RecForme
+//
+//  Created by Hubert Godfroy on 12/06/12.
+//  Copyright (c) 2012 Mines de Nancy. All rights reserved.
+//
+
+#ifndef RecForme_Tests_h
+#define RecForme_Tests_h
+
+
+
+#endif
